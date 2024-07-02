76a0f632a86ea39fb183140d279d18dd677eebb0
Jairo Durango <cartuchoxl@hotmail.com>
2024-07-01 22:00:45 -0500

    Mi foro

---
+++ src/main/java/com/aluracursos/forohub/errores/ValidacionIntegridad.java
@@ -0,0 +1,10 @@
+package com.aluracursos.forohub.errores;
+
+
+
+public class ValidacionIntegridad extends RuntimeException {
+    public ValidacionIntegridad(String s) {
+
+        super(s);
+    }
+}
